1973860cd15cbce0553b7c64cd30185bfd929934
Mugisha-isaac <mugishaisaac2001@gmail.com>
2021-11-22 21:46:49 +0200

    setting up model schema for queries in schema.js

---
+++ src/schema.js
@@ -0,0 +1,23 @@
+const {gql} = require('apollo-server');
+const typeDefs = gql`
+type Student{
+    id:ID!
+    email:String!
+    dept:String!
+    enrolled:Boolean
+}
+
+type Query{
+    enrollment:[Student!]
+    students:[Student!]
+    student(id:ID!): Student
+}
+
+type Mutation{
+      registerStudent(email:String!, fullname:String!,dept:String!):Student!
+      enroll(id:ID!):Student
+}`
+
+module.exports ={
+    typeDefs
+}
